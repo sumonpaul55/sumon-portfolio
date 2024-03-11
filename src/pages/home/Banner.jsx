a832eb254ad6c1ab94c53dc00c195ba1ec5a49d5
sumonpaul55 <sumpaul100@gmail.com>
2024-03-12 00:28:15 +0600

    resume updated

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -12,30 +12,32 @@ const Banner = () => {
                     <div className='flex flex-col md:flex-row md:items-center gap-16 md:gap-5'>
                         <div className='text-center md:text-start md:w-1/2'>
                             <div data-aos="fade-right" className='overflow-hidden'>
-                                <h2 className='text-xl md:text-3xl lg:text-4xl font-bold mb-6'>Hi, I am <span className='text-pink-500'>Sumon Chandra Paul</span></h2>
+                                <h2 className='text-xl md:text-3xl lg:text-4xl font-bold pb-10'>Hi, I&apos;m <span className='text-pink-500'>Sumon Chandra Paul</span></h2>
                                 <TypeAnimation
                                     sequence={[
                                         // Same substring at the start will only be typed out once, initially
                                         "I am, " + `Front end Developer`,
-                                        1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                        1500, // wait 1s before replacing "Mice" with "Hamsters"
                                         "I am, " + 'React js Developer',
-                                        1000,
+                                        1500,
+                                        "I am, " + 'Next js Developer',
+                                        1500,
                                         "I am, " + 'Mern stack Developer',
-                                        1000,
+                                        1500,
                                     ]}
                                     wrapper="span"
-                                    speed={50}
+                                    speed={200}
                                     className='text-xl md:text-3xl font-bold'
                                     repeat={Infinity} />
                                 <div className=''>
-                                    <div className='my-10 border-slate-600 border max-w-[200px] mx-auto md:mx-0'></div>
+                                    <div className='mt-2 mb-5 border-slate-600 border max-w-[200px] mx-auto md:mx-0'></div>
                                     <p className='max-w-[400px] mx-auto md:mx-0 text-start'>As a React.js front-end developer, I excel in crafting dynamic websites with a keen emphasis on efficiency and a seamless user experience.</p>
                                     <div className='flex gap-4 md:gap-10 mt-5 justify-center md:justify-start'>
                                         <button className='px-4 lg:px-0 font-semibold lg:min-w-[200px] hover:bg-pink-800 duration-200 rounded-md bg-pink-600 py-1 md:py mt-5-2'>
                                             <a href={resume} download>Download Resume</a>
                                         </button>
                                         <button className='px-4 lg:px-0 font-semibold lg:min-w-[200px] hover:bg-pink-800 duration-200 rounded-md bg-pink-600 py-1 md:py mt-5-2'>
-                                            <a href="https://drive.google.com/file/d/1ww5z3TermAyzInt3qPy-99dqFcKwp5AJ/view?usp=sharing" target='_blank' rel="noreferrer">View Resume</a>
+                                            <a href="https://drive.google.com/file/d/17RveTVaFmyA6dtJKwrPjYOUwBXCIVjyE/view?usp=sharing" target='_blank' rel="noreferrer">View Resume</a>
                                         </button>
                                     </div>
                                 </div>
